99a829a893ae36bd75016eaa2e0d1606197bb12f
Oleg Ilyin <ilynrabota@yandex.ru>
2024-04-14 18:34:57 +0500

    create tests

--- main_test.go
+++ main_test.go
@@ -1,22 +1,53 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "strconv"
-    "strings"
-    "testing"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+// TestMainHandlerWhenOk тестирует корректность запроса, код ответа 200 тело ответа не пустое.
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Result().StatusCode)
+	require.NotEmpty(t, responseRecorder.Body)
+
+}
+
+// TestMainHandlerWhenWrongCityValue тестирует запрос неподдерживаемого города.
+func TestMainHandlerWhenWrongCityValue(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=Imperial-City", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Result().StatusCode)
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+
+}
+
+// TestMainHandlerWhenCountMoreThanTotal тестирует запрос с числом кафе больше существующего.
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
+	totalCount := len(cafeList["moscow"])
+	req := httptest.NewRequest("GET", "/cafe?count=8&city=moscow", nil)
 
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-    // здесь нужно добавить необходимые проверки
-} 
+	list := strings.Split(responseRecorder.Body.String(), ",")
+
+	require.Equal(t, http.StatusOK, responseRecorder.Result().StatusCode)
+	assert.Equal(t, totalCount, len(list))
+
+}
